de5af7e59dbee68cb21dc83346f517078605c242
sykim@woowahan.com <sykim@sykim-MacBook-Pro.local>
2015-11-23 21:39:09 +0900

    [TDD] 2장 완료

--- TDD/src/main/java/Dollar.java
+++ TDD/src/main/java/Dollar.java
@@ -12,8 +12,8 @@ public class Dollar {
 		this.amount = amount;	
 	}
 	
-	void times(int multiplier) {
-		amount *= multiplier;
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
 	}
 
 }
