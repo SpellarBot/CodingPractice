78e6cb5adeb0f655f917216860d2fc22c0c14b57
philo-grammer <great1106@gmail.com>
2015-12-02 01:56:40 +0900

    연습문제 1.19 완료

--- SICP_Clj/20151130/1-2-4_ex.clj
+++ SICP_Clj/20151130/1-2-4_ex.clj
@@ -60,3 +60,30 @@
 (times 3 4)
 (times 3 5)
 (times 3 6)
+
+
+;------------------------------
+; 연습문제 1.19
+;------------------------------
+
+; Ashal's Solution
+
+; 이해가 안됨..
+
+(defn fib-iter [a b p q count]
+  (cond (= count 0) b
+        (even? count) (fib-iter a
+                                b
+                                (+ (* p p) (* q q))
+                                (+ (* 2 p q) (* q q))
+                                (/ count 2))
+        :else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1))))
+
+(defn fib [n]
+  (fib-iter 1 0 0 1 n))
+
+(map fib (range 11))
