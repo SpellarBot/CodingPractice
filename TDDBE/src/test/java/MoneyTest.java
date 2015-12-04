9120ca49fd37c737ed6ca13f524c9150d011e907
philo-grammer <great1106@gmail.com>
2015-12-04 22:17:20 +0900

    11장 완료

--- TDDBE/src/test/java/MoneyTest.java
+++ TDDBE/src/test/java/MoneyTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
 import tddbe.Money;
-import tddbe.Franc;
 import org.junit.Test;
 
 // < To do list >
@@ -20,7 +19,7 @@ import org.junit.Test;
 // Equal object -> 달러가 아닌게 들어오면?
 // Dollar/Franc 중복
 // 공용 times
-// testFrancMultiplication을 지워야 할까?
+// (V) testFrancMultiplication을 지워야 할까?
 
 // 1.red
 // 2.green
@@ -37,34 +36,15 @@ public class MoneyTest {
 
 	@Test
 	public void testEquality() {
-		
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-
-		Money franc = Money.franc(5);
-		assertTrue(Money.franc(5).equals(franc));
-		assertFalse(Money.franc(6).equals(franc));
-
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
-
 	}
 	
-	@Test
-	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
-
 	@Test
 	public void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
-	}
-
-	@Test
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
 	}
 
 }
