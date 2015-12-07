9059b876bab71626436bbb911027911bb5bc41bb
philo-grammer <great1106@gmail.com>
2015-12-07 23:16:53 +0900

    약수 찾기

---
+++ SICP_Clj/20151204/1-2-6.clj
@@ -0,0 +1,27 @@
+(ns test)
+(remove-ns 'test)
+(ns test)
+
+;---------------------
+; 1.2.6 연습 : 소수 찾기
+;---------------------
+
+; 약수 찾기
+
+(defn square [n] (* n n))
+
+(defn divides? [a b]
+  (= (mod b a) 0))
+
+(defn find-divisor [n test-divisor]
+  (cond (> (square test-divisor) n) n
+        (divides? test-divisor n) test-divisor
+        :else (find-divisor n (+ test-divisor 1))))
+
+(defn smallest-divisor [n]
+  (find-divisor n 2))
+
+(defn prime? [n]
+  (= n (smallest-divisor n)))
+
+(prime? 5)
